255265fc07b0e7062bc98c4d90158a14421bdfef
Cyvid7-Darus10 <cyvid@pop-os.localdomain>
2020-11-26 20:43:35 +0800

    Lab 11 update

--- Laboratory Answers/Lab_11/LabExercise-StatesOfMatter.py
+++ Laboratory Answers/Lab_11/LabExercise-StatesOfMatter.py
@@ -27,12 +27,17 @@ class Solid(State):
         self.__matter = matter
 
     def compress(self):
-        pass
+        print("Compressing")
+
     def release(self):
+        print("Releasing")
         self.__matter.changeState(Liquid(self.__matter))
+    
     def cool(self):
-        pass
+        print("Cooling")
+    
     def heat(self):
+        print("Heating")
         self.__matter.changeState(Liquid(self.__matter))
 
     def __str__(self):
@@ -43,12 +48,19 @@ class Liquid(State):
         self.__matter = matter
 
     def compress(self):
+        print("Compressing")
         self.__matter.changeState(Solid(self.__matter))
+    
     def release(self):
+        print("Releasing")
         self.__matter.changeState(Gas(self.__matter))
+    
     def cool(self):
+        print("Cooling")
         self.__matter.changeState(Solid(self.__matter))
+    
     def heat(self):
+        print("Heating")
         self.__matter.changeState(Gas(self.__matter))
     
     def __str__(self):
@@ -59,43 +71,60 @@ class Gas(State):
         self.__matter = matter
 
     def compress(self):
+        print("Compressing")
         self.__matter.changeState(Liquid(self.__matter))
+    
     def release(self):
-        return self
+        print("Releasing")
+
     def cool(self):
+        print("Cooling")
         self.__matter.changeState(Liquid(self.__matter))
+    
     def heat(self):
-        return self
+        print("Heating")
     
     def __str__(self):
         return "Gaseous"
 
 class Matter:
-    def __init__(self, name:str):
+    def __init__(self, name: str):
         self.__name = name
         self.__state =  Liquid(self) #change this to the appropriate initial state (liquid)
+    
     def changeState(self, newState):
         self.__state = newState
+    
     def compress(self):
         self.__state.compress()
+    
     def release(self):
         self.__state.release()
+    
     def cool(self):
         self.__state.cool()
+    
     def heat(self):
         self.__state.heat()
+    
     def __str__(self):
         return "%s is currently a %s" % (self.__name, self.__state) #formatting strings just like you format strings in C
 
 def main():
     a = Matter("Water")
-    print (a)
+    print(a)
     a.compress()
-    print (a)
+    print(a)
     a.heat()
-    print (a)
+    print(a)
     a.heat()
-    print (a)
+    print(a)
+    a.cool()
+    print(a)
+    a.cool()
+    print(a)
+    a.compress()
+    print(a)
 
 if __name__ == "__main__":
     main()
